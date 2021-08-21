816380bfa2ccf5be3dc2264a84869955b037c8c1
Yukiman-byde <yukiman8827@icloud.com>
2021-08-21 16:44:48 +0900

    最低限のDB設定及びREACTとmaterial-uiのインストールが完了

--- app/Display.php
+++ app/Display.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Display extends Model
 {
-    //
+    //  protected $fillable = ['name', 'description', 'video', 'thumbnail', 'quize_id', 'category_id'];
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'DisplayController@index')->name('display.name');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
